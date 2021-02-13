63b82dea720af159ed3854dfd367ca0e3ab23666
AndrewDilman <AndrewDilman@github.com>
2021-02-14 00:05:14 +0700

    Оптимизация пуль врагов
    -включил таймер удаления (4 сек)
    -правильно подключил нод VisibilityNotifier2D (Если пуля улетела за экран, она удаляется).
    -в psd босса я попробовал нарисовать глазик
    
    Про оптимизацию: Сперва думал обойтись одним нодом отслеживания экрана, но потом подключил таймер и производительно тоже подросла. Не очень понимаю как это работает, т.к. нод удаляет пулю если она за экраном; и таймер, в теории, просто может не успевать, но его включение каким-то образом тоже помогло.

--- Project/Scripts/Enemys/EBullet.gd
+++ Project/Scripts/Enemys/EBullet.gd
@@ -30,3 +30,7 @@ func _on_Area2D_area_entered(area):
 	if area.is_in_group("Player"):
 		#queue_free()
 		pass
+
+
+func _on_VisibilityNotifier2D_viewport_exited(viewport):
+	queue_free()
--- Project/Scripts/Monitoring.gd
+++ Project/Scripts/Monitoring.gd
@@ -1,6 +1,13 @@
 extends Node2D
 var fps
 
+func _input(event):
+	if event.is_action_pressed("HideFps"):
+		if $Control.visible == true:
+			$Control.hide()
+		elif $Control.visible == false:
+			$Control.show()
+
 func _process(delta):
 	fps = Engine.get_frames_per_second()
 	$Control/Counter.text = str(fps)

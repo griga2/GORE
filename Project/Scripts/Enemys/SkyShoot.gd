822a6b1cbae21921b63e1dce1073b18db0c46e95
griga2 <griga2999wow@gmail.com>
2021-02-16 23:15:41 +0700

    Реализовал подвижность лазера

--- Project/Scripts/Enemys/SkyShoot.gd
+++ Project/Scripts/Enemys/SkyShoot.gd
@@ -1,6 +1,14 @@
-extends Node2D
+extends KinematicBody2D
 
+func _process(delta):
+	Moving(delta)
+	
+var speed = 300
 
+func Moving(delta):
+	var MoveVec = GLOBAL.PlayerPref.global_position - global_position
+	MoveVec = MoveVec.normalized()
+	move_and_slide(MoveVec*speed)
 
 func _ready():
 	$AnimPlayer.play("ready")

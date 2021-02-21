e918e225f771dbcd101cf95b5c894d76f88d2837
griga2 <griga2999wow@gmail.com>
2021-02-21 19:51:14 +0700

    Создал индекатор буйства

--- Project/Scripts/Player/Player_Scene.gd
+++ Project/Scripts/Player/Player_Scene.gd
@@ -5,6 +5,12 @@ onready var gunTimer = $GunTimer
 export (PackedScene) var bulletPref
 var clicked = false
 
+var screen_size 
+
+func _process(delta):
+	update()
+func _ready():
+	screen_size = get_viewport_rect().size
 
 func _input(event):
 	if event.is_action_pressed("shoot"):
@@ -39,3 +45,9 @@ func Spawn_Bullet():
 		bullet.global_position = $Player.current_hand.get_node("ShootPos").global_position
 		bullet._activate()
 #		$ShootSound.play()
+
+
+func _draw():
+	var Player = GLOBAL.PlayerPref
+	draw_rect(Rect2(screen_size.x/2*-1+Player.position.x,screen_size.y/2*-1+Player.position.y, screen_size.x,75) , Color(100,100,100), 20, false)
+	pass

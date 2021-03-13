17e2fc3f5e2c32e937769611be48773ebf5db3d1
AndrewDilman <67038546+AndrewDilman@users.noreply.github.com>
2021-03-13 19:58:55 +0700

    изменения с боссами и другое
    1) сделал копию босса с телеком и сделал анимацию в нём (пока она мне не до конца нравится)
    2) начал рисовать врага или босса глазик
    3) сделал полоску хп меньше и удобнее

--- Project/Scripts/globals/HP_Bar.gd
+++ Project/Scripts/globals/HP_Bar.gd
@@ -7,7 +7,7 @@ func _process(delta):
 
 func _draw():
 	var Player = GLOBAL.PlayerPref
-	draw_rect(Rect2(screen_size.x/2*-1+self.position.x, screen_size.y/2*-1+self.position.y, (screen_size.x-2)/100*GLOBAL.PlayerHP, 50) , Color(100,100,100), true)
+	draw_rect(Rect2(screen_size.x/2*-1+self.position.x, screen_size.y/2*-1+self.position.y, (screen_size.x-2)/100*GLOBAL.PlayerHP, 7) , Color(100,100,100), true)
 #	for i in range(0, screen_size.x/10):
 #		for j in range(1,30):
 #			draw_rect(Rect2(screen_size.x/2*-1+self.position.x+(i*10), screen_size.y/2*-1+self.position.y, 10, 50), Color (1,1,j))

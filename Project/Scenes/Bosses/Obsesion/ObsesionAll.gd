c48fa1b86e77ff28b5e0011be7b8e1f8542cc421
griga2 <griga2999wow@gmail.com>
2021-03-17 22:02:41 +0700

    нАчал писать насение урона

---
+++ Project/Scenes/Bosses/Obsesion/ObsesionAll.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+var BossHP = 100
+
+func _ready():
+	pass
+
+func BossCheck():
+	print("XYETA ", BossHP)
---
+++ Project/Scenes/Bosses/Obsesion/ObsesionBody.gd
@@ -0,0 +1,11 @@
+extends AnimationPlayer
+
+
+func _ready():
+	pass
+
+
+
+func _on_Area2D_area_shape_entered(area_id, area, area_shape, self_shape):
+	if (area.is_in_group("Player_bullet")):
+		$Boss_crunch.BossCheck()
